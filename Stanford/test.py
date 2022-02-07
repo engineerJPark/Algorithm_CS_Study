d91f2d0648db55ccff9f35fe1616867ccc544601
engineerJPark <jshackist@gmail.com>
2022-02-07 21:53:03 +0900

    week 3 end

---
+++ Stanford/test.py
@@ -0,0 +1,24 @@
+# import statistics
+
+# dic = {1:100, 2:300, 3:300}
+
+# key_list = []
+# value_list = []
+# for key, value in dic.items():
+#     key_list.append(key)
+#     value_list.append(value)
+# median = statistics.median(value_list)
+# for idx in range(len(value_list)):
+#     if value_list[idx] == median:
+#         pivot_idx = key_list[idx]
+#         break
+    
+# print(key_list)
+# print(value_list)
+# print(pivot_idx)
+
+
+arr = [1,2,3,4,5,6,7,8,9]
+
+print(arr[0:3])
+
